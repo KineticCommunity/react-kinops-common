8519f6e24e8b3054883e318efeadcf89335c8c60
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-02-19 13:30:31 -0600

    updated to properly set production env variable DTSK-150

--- webpack.production.js
+++ webpack.production.js
@@ -12,5 +12,9 @@ module.exports = {
     library: 'react-kinops-common',
     libraryTarget: 'umd',
   },
-  plugins: [],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+  ]
 };
